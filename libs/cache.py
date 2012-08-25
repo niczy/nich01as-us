3c08f9019b5c2ed97cebd13b2585c57aebfdca10
Xiao Zhang <zero891109@gmail.com>
2012-08-25 15:33:51 +0800

    Fix bug in cache

--- libs/cache.py
+++ libs/cache.py
@@ -8,22 +8,25 @@ from google.appengine.api import memcache
 import hashlib
 import configs
 
+
 def Cached(interval):
     """A decorator that automatically cache the return value of the decorated
-    function in memcache, using the serialization of arguments as key. """
+    function in memcache, using the serialization of function name and arguments as key. """
     
     def get_md5(unhased):
         hashed = hashlib.md5()
         hashed.update(unhased)
         return hashed.hexdigest()
     
-    def gen_key(*args):
-        return get_md5(','.join(map(lambda x: str(x), args)))
+    def gen_key(fn, *args):
+        str_args = map(lambda x: str(x), args)
+        str_args.append(fn.__name__)
+        return get_md5(','.join(str_args))
         
     def wrapper(fn):
         def inner_wrapper(*args):
             if not configs.CACHE: return fn(*args)
-            key = gen_key(*args)
+            key = gen_key(fn, *args)
             value = memcache.get(key)
             if not value:
                 value = fn(*args)
